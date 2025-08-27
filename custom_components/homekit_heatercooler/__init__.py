a7c5eb259472b1a01968c6cf072bcc47d711b715
kard8 <110168813+kard8@users.noreply.github.com>
2025-08-28 03:14:26 +0530

    Update __init__.py

--- custom_components/homekit_heatercooler/__init__.py
+++ custom_components/homekit_heatercooler/__init__.py
@@ -1,12 +1,4 @@
-"""HomeKit HeaterCooler integration."""
-from .const import DOMAIN
+DOMAIN = "homekit_heatercooler"
 
 async def async_setup(hass, config):
-    hass.data[DOMAIN] = {}
-    return True
-
-async def async_setup_entry(hass, entry):
-    return True
-
-async def async_unload_entry(hass, entry):
     return True
